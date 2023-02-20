a2d1ad3567d9a8b74c0f10bf020660e42b4bafef
kukon11674 <kukon11674@gmail.com>
2023-02-19 20:10:14 -0500

    first commit

---
+++ kukon_s3.py
@@ -0,0 +1,10 @@
+import boto3
+aws_resource = boto3.resource("s3")
+bucket = aws_resource.Bucket("kukons3")
+response = bucket.create(
+    ACL='public-read',
+    CreateBucketConfiguration={
+        'LocationConstraint': 'us-east-2'
+    },
+  
+)
